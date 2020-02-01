949ed67554b16338b470dd9d9d20e98593c4eebe
Exochos <exochos@gmail.com>
2020-02-01 03:06:40 +0000

    Geoffry code

--- pizza.py
+++ pizza.py
@@ -3,16 +3,39 @@
 #   import math library
 import math as m
 
-
 def main():
 
     print("This program computes the cost per square inch of a pizza.\n\n")
 
     # get the diameter and convert to radius
-    radius = (float(input("Enter the diameter of the pizza (in inches): "))/2)
+    #radius = (float(input("Enter the diameter of the pizza (in inches): "))
+    radius = input("Enter the diameter of the pizza (in inches): ")
+    while(True):
+        try:
+
+            radius = float(radius)
+            break
+        except:
+
+            print("Fuck You Geoff")
+            exit()
+
+    radius = radius / 2
 
     #get the price in cents
-    price = float(input("Enter the price of the pizza in cents: "))
+    price = (input("Enter the price of the pizza in cents: "))
+
+    while(True):
+        try:
+
+            price = float(price)
+            print(type(radius))
+            break
+        except:
+
+            print("Fuck You Geoff")
+            exit()
+
 
     # area is equal to radius^2 * pi
     area = m.pi * (radius ** 2)
